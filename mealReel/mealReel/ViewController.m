5a6f0cdbd64c686a02af34d7b1896f037b84fae8
Yating <yz1339@nyu.edu>
2015-12-11 02:04:04 -0500

    addting the pictureTakingView

--- mealReel/mealReel/ViewController.m
+++ mealReel/mealReel/ViewController.m
@@ -12,6 +12,9 @@
 
 @end
 
+AVCaptureSession *session;
+AVCaptureStillImageOutput *StillImageOutput;
+
 @implementation ViewController
 
 - (void)viewDidLoad {
@@ -19,9 +22,77 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    
+    session = [[AVCaptureSession alloc] init];
+    
+    [session setSessionPreset:AVCaptureSessionPresetPhoto];
+    
+    
+    AVCaptureDevice *inputDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    NSError *error;
+    
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:inputDevice error:&error];
+    
+    if ([session canAddInput:deviceInput]) {
+        
+        [session addInput:deviceInput];
+        
+    }
+    
+    AVCaptureVideoPreviewLayer *previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    
+    [previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    
+    CALayer *rootLayer = [[self view] layer];
+    
+    [rootLayer setMasksToBounds:YES];
+    
+    CGRect frame = frameForCapture.frame;
+    
+    [previewLayer setFrame:frame];
+    
+    [rootLayer insertSublayer:previewLayer atIndex:0];
+    
+    StillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    
+    NSDictionary *outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG, AVVideoCodecKey, nil];
+    
+    [StillImageOutput setOutputSettings:outputSettings];
+    
+    [session addOutput:StillImageOutput];
+    
+    [session startRunning];
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)takephoto:(id)sender {
+    AVCaptureConnection *videoConnection = nil;
+    for (AVCaptureConnection *connection in StillImageOutput.connections) {
+        for (AVCaptureInputPort *port in [connection inputPorts]) {
+            if ([[port mediaType] isEqual:AVMediaTypeVideo]) {
+                videoConnection = connection;
+                break;
+            }
+        }
+    }
+    [StillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer != NULL) {
+            NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+            UIImage *image = [UIImage imageWithData:imageData];
+            imageView.image = image;
+        }
+    //Store images in an array, send it to pictureView
+    //Find a way to pass that array around views
+        
+    }];
+}
+
 
 @end
